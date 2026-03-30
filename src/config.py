29d0c187837cf3dfcd68fd7347ecf4f339080467
Patrick Adams <patrick_aust@hotmail.com>
2026-03-30 10:38:17 +0200

    commit before rm old and new size_calibration

--- src/config.py
+++ src/config.py
@@ -2,4 +2,4 @@ from pathlib import Path
 
 # This locates the root relative to THIS config file
 PROJ_ROOT = Path(__file__).resolve().parent.parent
-DATA_DIR = PROJ_ROOT / "data"
+DATA_DIR = PROJ_ROOT / "data" 
